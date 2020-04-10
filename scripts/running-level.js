2b517d782eb72a49efe6afb71bced16621b68d4b
gdukes707 <Gdukes707@gmail.com>
2020-04-10 09:16:52 -0700

    makes small tweaks

--- scripts/running-level.js
+++ scripts/running-level.js
@@ -137,7 +137,7 @@ function createObstacles() {
         const positionX = 500;
         const newerObstacle1 = new component(80, 550, "./assets/Warp_pipe1.png", positionX + distanceAccumulator, Math.ceil(Math.random()*200) + 450, "image");
         obstacleArray.push(newerObstacle1);
-        const newerObstacle2 = new component(80, Math.ceil(Math.random()*200) + 200, "./assets/Warp_pipe.png", positionX + distanceAccumulator, 0, "image");
+        const newerObstacle2 = new component(80, Math.ceil(Math.random()*200) + 160, "./assets/Warp_pipe.png", positionX + distanceAccumulator, 0, "image");
         obstacleArray.push(newerObstacle2);
         distanceAccumulator += positionX;
     }
